44132d8ff01f42ff01f887b183f33596b388f70c
Reihaneh Kalhor <reihaneh.mohammad.bagher.khan.kalhor@vub.be>
2019-11-15 17:56:14 +0100

    -

--- dbUtilities.js
+++ dbUtilities.js
@@ -15,51 +15,6 @@ const con = MySQL.createConnection({
   });
 
 
-// users Function
-function createAccount(userName , password) {}
-function login(userName,password){
-  if (err) throw err;
-    var sql = "INSERT INTO `users`(`user_id`, `user_name`, `password`, `name`, `last_name`, `created_date`)" + 
-    " VALUES (?,?,?,?,?,?)";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-    });
-  });
-function addRelative(){}
-function addUser(){
-  con.connect(function(err) {
-    if (err) throw err;
-    var sql = "INSERT INTO `users`(`user_id`, `user_name`, `password`, `name`, `last_name`, `created_date`)" + 
-    " VALUES (?,?,?,?,?,?)";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-    });
-  });
-}
-
-function getShoppingList(userId){
-  con.connect(function(err) {
-    if (err) throw err;
-    var sql = "SELECT * FROM shoppinglist a join shoppinglistdetails b on a.si_id = b.sl_id where a.owner_id = ?";
-    con.query(sql, [userId] , function (err, result) {
-      if (err) throw err;
-      return result;
-    });
-  });
-}
-
-function getUser(userName, password){
-  con.connect(function(err) {
-    if (err) throw err;
-    var sql = "select * from users where user_name = ? and password=?";
-    con.query(sql, [userName,password], function (err, result) {
-      if (err) throw err;
-      return result;
-    });
-  });
-}
 
 function insertProduct(barcode , name, description, manufacturer, image,size, brand, detectedDate){
   con.connect(function(err) {
@@ -77,15 +32,3 @@ function insertProduct(barcode , name, description, manufacturer, image,size, br
 
 module.exports.insertProduct = insertProduct;
 
-
-//barcode not null
-//name of product
-//brand
-//manufacture
-//name not null
-//description
-//detectedDate
-
-//Sam pass me this options and I insert them in database
-//I choose change database structure
-//just simplify it
