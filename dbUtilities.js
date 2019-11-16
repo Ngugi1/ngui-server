86ee0577d7b345f9999688ed23fd400a4fb54bee
Reihaneh Kalhor <reihaneh.mohammad.bagher.khan.kalhor@vub.be>
2019-11-16 15:41:17 +0100

    new functions

--- dbUtilities.js
+++ dbUtilities.js
@@ -15,18 +15,58 @@ const con = MySQL.createConnection({
   });
 
 
-
-function insertProduct(barcode , name, description, manufacturer, image,size, brand, detectedDate){
+// in this table -> the combination of barcode and status is primary key
+// it means if the product is detected and it is status is unbought we just update the amount and prevent to add 
+//the new row for it in table but also update the detected_date
+function insertProduct(barcode , name, description, manufacturer, image,size, brand, detectedDate, amount){
   con.connect(function(err) {
     if (err) throw err;
-    var sql = "INSERT INTO products(barcode, name, description, manufacturer, image, size, brand, detected_date) " + 
-    "VALUES (?,?,?,?,?,?,?,?)";
-    con.query(sql, [barcode , name, description, manufacturer, image,size, brand, detectedDate], function (err, result) {
+    var sql = "INSERT INTO products(barcode, name, description, manufacturer, image, size, brand, detected_date, amount) " + 
+    "VALUES (?,?,?,?,?,?,?,?,?)";
+    con.query(sql, [barcode , name, description, manufacturer, image,size, brand, detectedDate,amount], function (err, result) {
       if (err) throw err;
       return result;
     });
   });
 }
+
+//to update the amount of detected product and prevent to add more rows for one product
+function updateAmountofProduct(barcode, detected_date){
+  con.connect(function(err) {
+    if (err) throw err;
+    var sql = "update products set amount = amount + 1 and detected_date = ?  where barcode = ? and status = 1";
+    con.query(sql, [barcode , detected_date], function (err, result) {
+      if (err) throw err;
+      return result;
+    });
+  });
+}
+
+// status = 3 means deleted product
+function deleteProduct(barcode)
+{
+  con.connect(function(err) {
+    if (err) throw err;
+    var sql = "update products set status = 3 where barcode = ? and status = 1 ";
+    con.query(sql,[barcode], function (err, result) {
+      if (err) throw err;
+      return result;
+    });
+  });
+}
+
+function buyProduct(barcode)
+{
+  con.connect(function(err) {
+    if (err) throw err;
+    var sql = "update products set status = 2 where barcode = ? and status = 1 ";
+    con.query(sql,[barcode], function (err, result) {
+      if (err) throw err;
+      return result;
+    });
+  });
+}
+
 
 function showAllProducts(){
   con.connect(function(err) {
@@ -62,6 +102,21 @@ function showUNpurchasedProducts(){
 } 
 
 
+
+
+
+// status = 2 means purchased product
+function setProductAsPurchased(barcode, detected_date)
+{
+  con.connect(function(err) {
+    if (err) throw err;
+    var sql = "update products set status = 2 where barcode = ? and detected_date = ? ";
+    con.query(sql,[barcode , detected_date], function (err, result) {
+      if (err) throw err;
+      return result;
+    });
+  });
+}
 
 module.exports.insertProduct = insertProduct;
 module.exports.showAllProducts = showAllProducts;
